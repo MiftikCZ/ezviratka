5b074ad06edbc96de71e04b37bc554ed607f3781
MiftikCZ <memertv17@proton.me>
2023-05-31 18:44:47 +0200

    Fix blank screen in production

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: "./",
+  base: "./",
   plugins: [svelte()],
   "build": {
     outDir: "./docs"
