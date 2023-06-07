07cf49d8efd942d9114eab177a52c12340fc628b
MiftikCZ <memertv17@proton.me>
2023-06-07 07:31:09 +0200

    Add levels & New animals & Optimized ItemStructure

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,481 +1,112 @@
+import coin_image from "../assets/coin.png";
 
-import coin_image from "../assets/coin.png"
+import basicItems from "./items/basic";
+import kacheniItems from "./items/kacheni";
+import kamenaItems from "./items/kamena";
+import ledovaItems from "./items/ledova";
+import mikroItems from "./items/mikro";
 
-export type possibleViews = "list-of-items" | "list-of-animals";
+import kamena_doby_bg from "../assets/doby_bgs/kamena.png"
 
-function itemsPerSecond(seconds:number,count:number):[number,number] {
-    return [seconds,count]
+type ArrayToUnion<T extends string[]> = T[number] extends infer U ? U extends keyof any ? U : never : never;
+
+export type possibleViews = "list-of-items" | "list-of-animals" | "list-of-levels";
+
+
+export function itemsPerSecond(
+  seconds: number,
+  count: number,
+): [number, number] {
+  return [seconds, count];
 }
+
+export function E(num:number,zeros:number) {
+  return num*(10**zeros)
+}
+
+interface levelsStructure {
+  [key: string]: {
+    cost?: number;
+    bghue: number,
+    bg?: string,
+    title: string,
+    items: ItemStructure;
+  };
+}
+
+export const levelsInfo: levelsStructure = {
+  "basic": {
+    bghue:111,
+    title:"základní",
+    bg: `url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAABfklEQVRYR8WXTQrCMBCFWzcVFHQneAi3rnShuBEv4jm8kjfwLkp3CgoWFGUCU9J0ZppJ/1xJmuZ982amSeLNcf2LlL/hPCm9kZ1TcpXkMCPH39fMjMdaAEocFuoEgBMHgO/5ZiL6RINCxLPJKLqvxiUX1A5I4r0DYPScAzDuuqBywMd69JhKQWsAduRaAJgPLnh1AeUAJa5JQQ6wP+3MdwBz4pYrZ38TAOY7IAHUFQcBaEOqBjDQHKDUqMKAT+7xdQTgIGoDuFVPcUsuqAG4T67koOSCCkASt0UQJn28zF/pWbxdLtS7oRstJQBzEICCwGfBAJyoDWcDcCnyAvAR4wSqIAoA3OEBFp9enppOLcyVIFRFGArSKIBkA+eSC2A7rXagLoCb5k4BqBorAFA7YtVRzHaESoFtvwjAbceU5RyUBMB1mLgdc/mmAEKih/W9TkQuiBZAupw0AhAavfeZMMQBLL7Gr2YA45OCXgGqWg+CUF1MtCloHYBKQ+jt+A8d+xNtbSee1wAAAABJRU5ErkJggg==)`,
+    items: basicItems,
+  },
+  "kamena": {
+    bghue:220,
+    title: "doba kamená",
+    bg: `url(${kamena_doby_bg})`,
+    cost: 75000,
+    items: kamenaItems,
+  },
+  "ledova": {
+    bghue: 190,
+    title: "doba ledová",
+    bg: `url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAABfElEQVRYR+2XwW3DMAxF6w0Ke4MC7RAdIffceumpC/SUBbpjC3QDB9kggQzQoCiK/9NtgBySkyNL/E+fNGUPb4ev84P6HedZ/12uT8d2rJnkDDyOUzM6TvXYcAfQDnj2W8sY61FKdcwqBRbgr+ICEsUddh+fVRFq+msBaA0X4L+Ebbq8FDcASNwLknHNrh9e9+9LCp6eX2B9IXEJgDZR5v3+fC/TaQBWnIVIAWTFGQgKIBK27dlruxFIAyCTo0Ayp3c2bFm71kAJzgboFWzZFRtDNrMCoIV61xEA66TECzuhZzl6XCW3rKMhgM01EhdgDYFAugBbxbMQLoCIsztGLVQc8eqsAmAKDYn17vdqYwHwnmtm99KoMr1fAxZHqj6gbyKA7MuLLcymD1jrPAD2TPAcSQNYq7bknnmd76aA7WgIDEGEAKg9I3F0eFUvJDYYEs8cxyV2zwnXgUgcWZpd2wBkA3hpyMQIP0x0cLTzTArpLyOmiKJC7DlxUwAX3ZkAVUNkBUkAAAAASUVORK5CYII=)`,
+    cost: 200000,
+    items: ledovaItems
+  },
+  "micro_animals": {
+    bghue: 20,
+    title: "doba mikro organismů",
+    bg: `url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAABgklEQVRYR82XO27CQBCG7QuAoojWRYSQKLgBJV2uk9wiB4iUy9BRpaKwhCIKJCoURQZRJxlLY+2u5+X1GuHOZmbnm38eNvly/vSbDXS9zBZZdb1mH8c9GyG3AMBBcL3ttp1Q0U+CEAHeV891wK/DQc2EI9NUMAFc/mV83aw7ZY/GADAtiow7QwTQnK1EqORnWbb6QQXQmsiFGD9OWkzV96l+xvVRMgAqONIgBKVYUoCH8ciL8VOdm3sOwgPALFzZrCUA3xAAoyMIBUECgCMYUyPE1bI3gNRA7kjVcMR2SwLgSgiyuZJpW40DkOSHZJoShAeEADDL0kLqPQUSQKqFJI6hBmCdBut2RDuyBG7dtNp3DRjaewDhj9wo9g3q+qubkBu7VBD3CzB07VtN6Ep6q+DeIrKs29i6S59lXg8Mlbm0RRuAoYKDaiaAWHktfuYSWA6LsZHUNf0xiX3ThY1NLTUVAINTn1vau54a7RDiZgBc6e4fAMj79oDUuKoCMV3fxecPIPZx8H/Up+oAAAAASUVORK5CYII=)`,
+    cost: E(1,6),
+    items: mikroItems
+  }
+  // "kacheni": {
+  //   bghue:190,
+  //   bg: `url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAABPElEQVRYR+2XMRKCMBBFzUW4AZaOnZWFlYWMPSdxvABXoHewsLaz9BjcwkrH6DJk2WQ3EYgFNBQw899+Nn8Xla42z9mY18MUU6MCIPE3ShBAvl2TnpWXq9vLXwFswpRqB4YQ93KAEk+SRGvXdW2tXINYxMUAWByEbaptoN4BOHGAAojy5O4LUROCA1Lx9meJDvCGcUEM7kA0AOlnGMyBXgC445dlmW76qqr0fXc/6Pt5cTTyQdwDXNLhU2AD4Gbb/PYJMCOIfMUpEXDAG6AtjquEQPHJAJ8w0k0YFaAoimYhoaoc3AEAsFkcHYBrKNdzyUBSnAOhAMZIdkzE/wGASkOOG3ZJWn0TRK4Q8gXC65l4H+CSMKQXOHEjihuB7wKZ7+nVWwIiEe7MAtfmKhENfUely5F/zRDpBDA58AJFs9LB1o2PMAAAAABJRU5ErkJggg==)`,
+  //   title: "vláda kachen",
+  //   cost: 200000,
+  //   items: kacheniItems,
+  // },
+};
+
+const levelsInfoKeys = Object.keys(levelsInfo)
+export type possibleLevels = ArrayToUnion<typeof levelsInfoKeys>;
 
 export interface ItemStructure {
-    [key:string]: {
-        title: string,
-        icon?: string,
-        type: "animal" | "material",
-        sell?: number,
-        produce?: {
-            [key:string]: [number,number]
-        },
-        cost?: {
-            [key:keyof ItemStructure]: number
-        }
-    }
+  [key: string]: {
+    title: string;
+    icon?: string;
+    type: "animal" | "material";
+    showAlways?: boolean,
+    sell?: number;
+    produce?: {
+      [key: string]: [number, number];
+    };
+    cost?: {
+      [key: keyof ItemStructure]: number;
+    };
+  };
 }
 
-export const coin = `<!-- https://www.flaticon.com/free-icons/coin --><img height="32px" class="coin" href="${coin_image}" >` 
+export const coin =
+  `<!-- https://www.flaticon.com/free-icons/coin --><img height="32px" class="coin" href="${coin_image}" >`;
 
-export function round(num:number):string {
-    return Math.floor(num).toLocaleString()
+export function round(num: number): string {
+  return Math.floor(num).toLocaleString();
 }
 
-function sellItems(sellFor: number, howMuchForThat: number):number {
-    return sellFor / howMuchForThat
+export function sellItems(sellFor: number, howMuchForThat: number): number {
+  return sellFor / howMuchForThat;
 }
 
 export interface backpackStructure {
-    [key:string]: number
+  [key: string]: number;
 }
 
 export const itemsInfo: ItemStructure = {
-    "seed": {
-        sell: sellItems(2,1),
-        title: "semínko",
-        icon: "🌱",
-        type: "material",
-        cost: {
-            "coin": 5
-        }
-    },
-    "water": {
-        sell: sellItems(2,1),
-        title: "voda",
-        icon: "💧",
-        type: "material",
-        cost: {
-            "coin": 5
-        }
-    },
-    "animal_seed": {
-        title: "zvířecí semínko",
-        icon: "🌱",
-        type: "material",
-        cost: {
-            "seed": 15,
-            "coin": 100,
-            "dirt": 10
-        }
-    },
-    "pet_license": {
-        title: "domácí zvíře license",
-        icon: "🪪",
-        type: "material",
-        cost: {
-            "coin": 150        
-        }
-    },
-    "dirt": {
-        title:"hlína",
-        cost: {
-            "coin": 9
-        },
-        type: "material"
-    },
-    "grass": {
-        title: "tráva",
-        type: "material",
-        cost: {
-            "dirt": 1,
-            "seed": 1,
-        }
-    },
-    "wood": {
-        title: "dřevo",
-        icon: "🪵",
-        type: "material",
-        cost: {
-            "coin": 10,
-            "tree": 0.5,
-        }
-    },
-    "tree": {
-        sell: sellItems(170,1),
-        title: "strom",
-        icon: "🌲",
-        type: "material",
-        cost: {
-            "coin": 10,
-            "poop": 25,
-            "seed": 20
-        }
-    },
-    "fire": {
-        title: "oheň",
-        icon: "🔥",
-        type: "material",
-        cost: {
-            "wood": 2,
-            "coin": 10,
-            "iron": 1
-        }
-    },
-    "steak": {
-        title: "steak",
-        icon: "🥩",
-        sell: sellItems(150,1),
-        type: "material",
-        cost: {
-            "cow": 1,
-            "coin": 10,
-            "fire": 2
-        }
-    },
-    "stone": {
-        title: "kámen",
-        sell: sellItems(18,1),
-        type: "material",
-        icon: "🪨",
-        cost: {
-            "grass": 5,
-            "coin": 25
-        }
-    },
-    "pickaxe": {
-        sell: sellItems(50,1),
-        title: "krumpáč",
-        icon: "🔨",
-        type: "material",
-        cost: {
-            "wood": 10,
-            "stone": 3,
-            "coin": 10
-        }  
-    },
-    "stone_pickaxe": {
-        sell: sellItems(110,1),
-        title: "kamenný krumpáč",
-        icon: "🔨",
-        type: "material",
-        cost: {
-            "pickaxe": 1,
-            "stone": 10
-        }  
-    },
-    "iron": {
-        sell: sellItems(200,1),
-        title: "železo",
-        icon: "🪨",
-        type: "material",
-        cost: {
-            "stone_pickaxe": 1,
-            "dirt": 2,
-            "stone": 10
-        }  
-    },
-    "gold": {
-        sell: sellItems(800,1),
-        title: "zlato",
-        icon: "🪨",
-        type: "material",
-        cost: {
-            "iron": 10,
-            "stone_pickaxe": 2,
-        }
-    },
-    "cat": {
-        title: "kočka",
-        icon: "🐱",
-        type: "animal",
-        cost: {
-            "animal_seed": 1,
-            "pet_license": 1,
-            "coin": 250
-        },
-        produce: {
-            "mouse": itemsPerSecond(120,1)
-        }
-    },
-    "duck": {
-        title:"kachna",
-        icon: "🦆",
-        type:"animal",
-        cost: {
-            "animal_seed": 1,
-            "water": 100,
-            "coin": 250
-        },
-        produce: {
-            "duckling": itemsPerSecond(360,1),
-        }
-    },
-    "duckling": {
-        title:"kachňátko",
-        icon: "🦆",
-        sell: sellItems(5,1),
-        type:"animal",
-        produce: {
-            "grass": itemsPerSecond(180,1),
-        }
-    },
-    "mouse": {
-        sell: sellItems(200,30),
-        title: "myš",
-        icon: "🐭",
-        type: "animal",
-        produce: {
-            "worm": itemsPerSecond(60,1)
-        }
-    },
-    "worm": {
-        sell: sellItems(160,300),
-        title: "červík",
-        icon: "🐛",
-        type: "animal",
-        produce: {
-            "coin": itemsPerSecond(180,1),
-            "dirt": itemsPerSecond(60,1)
-        }
-    },
-    "poop": {
-        sell: sellItems(3,2),
-        title: "hnůj",
-        icon: "💩",
-        type: "material"
-    },
-    "fish": {
-        title:"ryba",
-        icon: "🐟",
-        type:"animal",
-        cost: {
-            "animal_seed": 1,
-            "water": 50,
-            "coin": 75
-        },
-        produce: {
-            "water": itemsPerSecond(60,1),
-        }
-    },
-    "big_fish": {
-        title:"huge ryba",
-        icon: "🐟",
-        type:"animal",
-        cost: {
-            "animal_seed": 1,
-            "water": 100,
-            "coin": 100,
-            "fish": 5
-        },
-        produce: {
-            "coin": itemsPerSecond(8,1),
-        }
-    },
-    "egg": {
-        title: "vajco",
-        icon: "🥚",
-        sell: sellItems(2,3),
-        type: "material"
-    },
-    "ice": {
-        title: "led",
-        icon: "🧊",
-        sell: sellItems(5,4),
-        type: "material"
-    },
-    "bird_kosak": {
-        title:"kosák",
-        icon: "🐦",
-        type:"animal",
-        cost: {
-            "animal_seed": 1,
-            "water": 3
-        },
-        produce: {
-            "coin": itemsPerSecond(60,1),
-        }
-    },
-    "bird_lednacek": {
-        title:"ledňáček",
-        icon: "🐦",
-        type:"animal",
-        cost: {
-            "animal_seed": 1,
-            "water": 3,
-            "coin": 400
-        },
-        produce: {
-            "coin": itemsPerSecond(22,1),
-            "ice": itemsPerSecond(8,1)
-        }
-    },
-    "bird_vrabec": {
-        title:"vrabčák",
-        icon: "🐦",
-        type:"animal",
-        cost: {
-            "animal_seed": 2,
-            "water": 5,
-            "seed": 10,
-            "coin": 1500
-        },
-        produce: {
-            "gold": itemsPerSecond(60*60,1)
-        }
-    },
-    "chicken": {
-        title: "slepice",
-        icon: "🐔",
-        type: "animal",
-        produce: {
-            "egg": itemsPerSecond(5,1),
-            "seed": itemsPerSecond(10,1)
-        },
-        cost: {
-            "animal_seed": 1,
-            "seed": 5,
-            "coin": 100
-        }
-    },
-    "pig": {
-        title: "prase",
-        icon: "🐷",
-        type: "animal",
-        produce: {
-            "coin": itemsPerSecond(20,1)
-        },
-        cost: {
-            "coin": 170,
-            "grass": 5,
-            "fire": 1,
-            "animal_seed": 3
-        }
-    },
-    "cow": {
-        title: "kráva",
-        icon: "🐮",
-        type: "animal",
-        produce: {
-            "poop": itemsPerSecond(10,1)
-        },
-        cost: {
-            "grass": 10,
-            "animal_seed": 1,
-            "coin": 150,
-            "water": 10
-        }
-    },
-    "penguin": {
-        title: "tučňák",
-        icon: "🐧",
-        type: "animal",
-        produce: {
-            "coin": itemsPerSecond(60,1)
-        },
-        cost: {
-            "ice": 50,
-            "water": 1,
-            "animal_seed": 1,
-            "coin": 350
-        }
-    },
-    "golden_pig": {
-        title: "zlaté prase",
-        icon: "🪙",
-        type: "animal",
-        produce: {
-            "coin": itemsPerSecond(1,1)
-        },
-        cost: {
-            "iron": 1,
-            "animal_seed": 3,
-            "gold": 5,
-            "pig": 2,
-            "coin": 1000
-        }
-    },
-    "iron_chicken": {
-        title: "železná slepice",
-        icon: "🐔",
-        type: "animal",
-        produce: {
-            "iron": itemsPerSecond(10,1)
-        },
-        cost: {
-            "iron": 1,
-            "animal_seed": 3,
-            "gold": 5,
-            "chicken": 2,
-            "coin": 500
-        }
-    },
-    "tree_nut": {
-        title: "oříškovník",
-        icon: "🌲",
-        type: "animal",
-        produce: {
-            "nut": itemsPerSecond(60,1)
-        },
-        cost: {
-            "tree": 1,
-            "nut": 10,
-            "coin": 100
-        }
-    },
-    "icecream": {
-        title: "zmrzlina",
-        icon: "🍦",
-        type: "material",
-        cost: {
-            "ice": 50,
-            "coin": 2000,
-        }  
-    },
-    "bird_parrot": {
-        title:"papoušek",
-        icon: "🐦",
-        type:"animal",
-        cost: {
-            "animal_seed": 1,
-            "pet_license": 1,
-            "cage": 1,
-            "nut": 5,
-            "coin": 1000
-        },
-        produce: {
-            "feather": itemsPerSecond(15,1)
-        }
-    },
-    "cage": {
-        title: "klec",
-        icon: "📦",
-        type: "material",
-        cost: {
-            "iron": 3,
-            "coin": 500,
-        }  
-    },
-    "nut": {
-        title: "oříšek",
-        icon: "🌰",
-        type: "material",
-        cost: {
-            "seed": 10,
-            "grass": 1,
-            "tree": 0.5,
-            "coin": 500,
-        }  
-    },
-    "feather": {
-        sell: sellItems(20,10),
-        title: "pírko",
-        icon: "🪶",
-        type: "material"
-    },
-    "bed": {
-        title: "postel",
-        icon: "🛏️",
-        type: "material",
-        cost: {
-            "feather": 100,
-            "coin": 150   
-        }
-    },
-}
+  ...levelsInfo.basic.items,
+  ...levelsInfo.kamena.items,
+  ...levelsInfo.ledova.items,
+};
